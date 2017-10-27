3fff8d4a5547c755bb68d97bd29796d047e8833d
cloudera <cloudera@edge1.feetytoes.com>
2017-10-27 09:05:03 -0400

    fixed get logic

--- src/main/scala/spark-hbase/HBaseTest.scala
+++ src/main/scala/spark-hbase/HBaseTest.scala
@@ -1,27 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package spark.examples.hbase
+package spark.examples.hbase 
 
 import org.apache.spark._
 import org.apache.spark.rdd.NewHadoopRDD
 import org.apache.hadoop.hbase.{HBaseConfiguration, HTableDescriptor}
-import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.{CellUtil,TableName}
+import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.rdd.RDD
+
+import java.io.File
+import java.io.FileInputStream
+import org.apache.hadoop.fs.FileUtil
+import org.apache.hadoop.conf.Configuration
+import java.io.StringWriter
 
 object HBaseTest {
   def main(args: Array[String]) {
@@ -33,21 +25,52 @@ object HBaseTest {
 
     // Other options for configuring scan behavior are available. More information available at 
     // http://hbase.apache.org/apidocs/org/apache/hadoop/hbase/mapreduce/TableInputFormat.html
-    conf.set(TableInputFormat.INPUT_TABLE, args(0))
+    conf.set(TableInputFormat.INPUT_TABLE, "bridge:repo") 
+    val osw = new StringWriter()
+    Configuration.dumpConfiguration(conf,osw)
+    println("Start of conf dump")
+    //println(osw.toString)
+    println("End of print dump")
 
     // Initialize hBase table if necessary
     val admin = new HBaseAdmin(conf)
-    if(!admin.isTableAvailable(args(0))) {
-      val tableDesc = new HTableDescriptor(args(0))
+    if(!admin.isTableAvailable("bridge:repo")) {
+      val tableDesc = new HTableDescriptor("bridge:repo")
       admin.createTable(tableDesc)
     }
 
-    val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat], 
+    val hRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat], 
       classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
       classOf[org.apache.hadoop.hbase.client.Result])
+    // rdd returns tuple of ImmutableBytesWritable, Result
+    // for ease of viewing data, convert to new RDD with String, String i.e. rowKey(in String), value(in String)
+    // we are stripping out the timestamp value from the rowkey as well
+    val hBaseRDD = hRDD.map(tuple => tuple._2).map(result => (Bytes.toString(result.getRow()).split(" ")(0), Bytes.toString(result.value)))
+   
+    // lets output one 
+    hBaseRDD.take(1).foreach( x => println("Sample data from newApiHadoopRDD: " + x._1 + " XXX " + x._2))
 
-    println("RDD Count" + hBaseRDD.count())
+    
+    // function to retrieve rows from hbase table
+    def getRec (htable: Table, rowKey : String ) :  (String, String) = {
+      var rkey = new Get(Bytes.toBytes(rowKey))
+      var rval = htable.get(rkey)
+      (rowKey, Bytes.toString(rval.value))
+    }
 
-    System.exit(0)
+    val readRDD = hBaseRDD.mapPartitions(iter => {
+      var conf = HBaseConfiguration.create()
+      var inconf = new File("/etc/hbase/conf/hdfs-site.xml")
+      var inHbase = new FileInputStream(inconf)
+      conf.addResource(inHbase, "hbase-site.xml")
+      conf.set(TableInputFormat.INPUT_TABLE, "bridge:repo")
+      var connection = ConnectionFactory.createConnection(conf)
+      var htable = connection.getTable(TableName.valueOf( Bytes.toBytes("bridge:repo") ) )
+      println("Connected!")
+      iter.map{ case(x,y) => getRec(htable,x) }
+      }
+    )
+    println("Count" + readRDD.count())
+    readRDD.take(1).foreach(x => println("Sample data from gets: " + x._1 + " YYY " + x._2))
   }
 }
